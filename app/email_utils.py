c1582cf7403fe85d55cb03cb4121642134663022
leor-gh <59747045+leor-gh@users.noreply.github.com>
2024-04-27 23:36:27 -0700

    Relax the string matching to get the Postfix queue id from the headers

--- app/email_utils.py
+++ app/email_utils.py
@@ -1300,14 +1300,11 @@ def get_queue_id(msg: Message) -> Optional[str]:
         return
 
     # received_header looks like 'from mail-wr1-x434.google.com (mail-wr1-x434.google.com [IPv6:2a00:1450:4864:20::434])\r\n\t(using TLSv1.3 with cipher TLS_AES_128_GCM_SHA256 (128/128 bits))\r\n\t(No client certificate requested)\r\n\tby mx1.simplelogin.co (Postfix) with ESMTPS id 4FxQmw1DXdz2vK2\r\n\tfor <jglfdjgld@alias.com>; Fri,  4 Jun 2021 14:55:43 +0000 (UTC)'
-    search_result = re.search("with ESMTPS id [0-9a-zA-Z]{1,}", received_header)
+    search_result = re.search("\sid\s([0-9a-zA-Z]{1,})", received_header)
     if not search_result:
         return
 
-    # the "with ESMTPS id 4FxQmw1DXdz2vK2" part
-    with_esmtps = received_header[search_result.start() : search_result.end()]
-
-    return with_esmtps[len("with ESMTPS id ") :]
+    return search_result.group(1)
 
 
 def should_ignore_bounce(mail_from: str) -> bool:
